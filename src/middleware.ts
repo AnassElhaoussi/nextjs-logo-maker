6164d8e96128cced5299bbed84415eb2e3d50b53
anasselhaoussi <anous.elhaoussi@gmail.com>
2023-06-13 15:41:07 +0200

    fix: getting the request body by adding an await call (readble strem object decoding)

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,21 +4,24 @@ import { ZodError } from "zod"
 import { ReqBodyType } from "./types";
 
 // Middleware function for user credentials validation (fires before the request is sent)
-export function middleware(req: Request) {
-        const { valid, error }: {
-            valid: boolean,
-            error: ZodError<{ email: string, password: string }> | null
-        } = isAuthValid(req.body as ReqBodyType)
-        // Checking if the user's credentials are valid
-        if (!valid) {
-            return NextResponse.json({
-                status: 400,
-                message: "Invalid request!",
-                errors: error?.errors
-            })
-        } else {
-            return NextResponse.next()
-        }
+export async function middleware(req: Request) {
+    // Getting the request body
+    const body = await req.json()
+    const { valid, error }: {
+        valid: boolean,
+        error: ZodError<{ email: string, password: string }> | null
+    } = isAuthValid(body as ReqBodyType)
+    // Checking if the user's credentials are valid
+
+    if (!valid) {
+        return NextResponse.json({
+            status: 400,
+            message: "Invalid request!",
+            error
+        })
+    } else {
+        return NextResponse.next()
+    }
 }
 
 // Matching the middleware to /api/register
--- src/zod/index.ts
+++ src/zod/index.ts
@@ -1,4 +1,4 @@
-import { z } from 'zod'
+import {z} from 'zod'
 
 // creating zod schema
 export const schema = z.object({
