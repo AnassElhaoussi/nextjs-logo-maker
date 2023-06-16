42769615f2125fd098707ef48a05303e03577778
anasselhaoussi <anous.elhaoussi@gmail.com>
2023-06-16 20:04:29 +0200

    feat : using a middleware for authorization

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,31 +1,47 @@
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 import { isAuthValid } from "./zod/validator";
-import { ZodError } from "zod"
 import { ReqBodyType } from "./types";
+import { authOptions } from "./lib/auth";
+import { getServerSession } from "next-auth";
 
-// Middleware function for user credentials validation (fires before the request is sent)
-export async function middleware(req: Request) {
-    // Getting the request body
-    const body = await req.json()
-    const { valid, error }: {
-        valid: boolean,
-        error: string | null
-    } = isAuthValid(body as ReqBodyType)
-    // Checking if the user's credentials are valid
-
-    if (!valid) {
-        return NextResponse.json({
-            message: "Invalid request!",
-            error
-        }, {
-            status: 400
-        })
-    } else {
-        return NextResponse.next()
+export const getSessionData = async () => {
+    const session = await getServerSession(authOptions)
+    return {
+        authenticated: !!session,
+        session
     }
 }
 
-// Matching the middleware to /api/register
-export const config = {
-    matcher: '/api/register'
+// Middleware function for user credentials validation (fires before the request is sent)
+export async function middleware(req: NextRequest) {
+    if (req.nextUrl.pathname.startsWith('/api/register')) {
+        // Getting the request body
+        const body = await req.json()
+        const { valid, error }: {
+            valid: boolean,
+            error: string | null
+        } = isAuthValid(body as ReqBodyType)
+        // Checking if the user's credentials are valid
+
+        if (!valid) {
+            return NextResponse.json({
+                message: "Invalid request!",
+                error
+            }, {
+                status: 400
+            })
+        } else {
+            return NextResponse.next()
+        }
+    }
+    // Another middleware that fires whenever a user tries to access the dashboard page
+    if (req.nextUrl.pathname.startsWith('/dashboard')) {
+        // Getting the session data from the exported function
+        const sessionData = await getSessionData()
+        if (!sessionData.authenticated) {
+            // Redirecting the user to the /register route if he's not authenticated
+            return NextResponse.redirect(new URL('/register', req.nextUrl))
+        }
+        return NextResponse.next()
+    }
 }
